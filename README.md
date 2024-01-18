767d81b4e9432d1ef4b7f921842e98c1e359a377
Craig Moore <133659923+salty-144@users.noreply.github.com>
2024-01-18 19:34:54 +0000

    added features

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 First commit of project_weather files
+
+/_ openweathermap.org api key generated and url to openweather.org _/
+/_ custom key _/
+
+/_ Fetches info from url _/
+
+/_ api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} _/
+
+/_ tried to incorpate a 5 day weather alongside current weather, maybe a step too far _/
+
+/_ list of variables for location input box, search button, location/temperature entered by user _/
+/_ document.getElementById targets html element _/
+
+/\*const windSpeedElement = document.getElementById("wind");
+
+/_ forecast _/
+/_ const fiveDayForecast = document.getElementById("fiveDayForecast");_/
+
+/_ on user click if location is present that is = locationInput (if)
+location is useable fetchWeather(location) function is called _/
+
+/_ function fetchWeather _/
+/_ if location is good, url/location = const url _/
+
+    /* Fetch sends a get requests to the url requesting the const url info, responce is then sent
+
+back and converted into json, 2nd .then is a promise that has a responce holding the data that we need to
+display the location and temp \*/
+
+      /* .catch displays error message if the fetch url is invalid location */
+
+       /*fiveDayForecast = console.log(data.forecast.forecastDay);*/
--- js/main.js
+++ js/main.js
@@ -1,26 +1,13 @@
-/* openweathermap.org api key generated and url to openweather.org  */
-/* custom key */
 const apiKey = "da638781bf87ede35a9a383b9d6636ee";
-/* Fetches info from url */
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-/* api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} */
-
-/* tried to incorpate a 5 day weather alongside current weather, maybe a step too far */
-
-/* list of variables for location  input box, search button, location/temperature entered by user */
-/* document.getElementById targets html element */
 const locationInput = document.getElementById("locationInput");
 const searchButton = document.getElementById("searchButton");
 const locationElement = document.getElementById("location");
 const temperatureElement = document.getElementById("temperature");
-/*const windSpeedElement = document.getElementById("wind");8
+const windElement = document.getElementById("wind");
+const descriptionElement = document.getElementById("description");
 
-/* forecast */
-/* const fiveDayForecast = document.getElementById("fiveDayForecast");*/
-
-/* on user click if location is present that is = locationInput (if) 
-location is useable fetchWeather(location) function is called  */
 searchButton.addEventListener("click", () => {
   const location = locationInput.value;
   if (location) {
@@ -28,21 +15,18 @@ searchButton.addEventListener("click", () => {
   }
 });
 
-/* function fetchWeather */
 function fetchWeather(location) {
-  /* if location is good, url/location = const url  */
   const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
-  /* Fetch sends a get requests to the url requesting the const url info, responce is then sent 
-  back and converted into json, 2nd .then is a promise that has a responce holding the data that we need to 
-  display the location and temp */
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       locationElement.textContent = data.name;
       temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-      /*fiveDayForecast = console.log(data.forecast.forecastDay);*/
+      windElement.textContent = `${Math.round(data.wind.speed)}°kmh`;
+      descriptionElement.textContent = data.weather[0].description;
     })
 
-    /* .catch displays error message if the fetch url is invalid location */
-    .catch((error) => "Location not valid");
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
 }
