dcce919fa1b993e9db4980561da7108c1a8963b1
Craig Moore <craig-moore22@outlook.com>
2024-02-01 10:59:43 +0000

    added date/time/timeszone

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const windElement = document.getElementById("wind");
 const descriptionElement = document.getElementById("description");
 const timezoneElement = document.getElementById("timezone");
 const weatherIcon = document.querySelector(".icon img");
+const humidityElement = document.getElementById("humidity");
 
 searchButton.addEventListener("click", () => {
   const location = locationInput.value;
@@ -21,17 +22,23 @@ function fetchWeather(location) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      const date = convertTime(data.dt, data.timezone);
+      console.log(data);
       locationElement.textContent = data.name;
+      humidityElement.textContent = `${Math.round(data.main.humidity)}`;
       temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
       windElement.textContent = `${Math.round(data.wind.speed)}°kmh`;
       descriptionElement.textContent = data.weather[0].description;
-      timezoneElement.textContent = data.timezone;
+      timezoneElement.textContent = date;
       const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
       weatherIcon.setAttribute("src", iconURL);
-      console.log(data);
     })
 
     .catch((error) => {
       console.error("Error fetching weather data:", error);
     });
+}
+
+function convertTime(seconds, timezone) {
+  return new Date(seconds * 1000 + timezone * 1000);
 }
